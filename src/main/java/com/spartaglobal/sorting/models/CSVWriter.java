91602e3e7b83f714f76ce47c35e1f54235826255
Lewis <lewisthornton@btinternet.com>
2021-09-30 13:38:17 +0100

    Neatened up CSVWriter

--- src/main/java/com/spartaglobal/sorting/models/CSVWriter.java
+++ src/main/java/com/spartaglobal/sorting/models/CSVWriter.java
@@ -13,16 +13,6 @@ public class CSVWriter {
 
     // Implement singleton generator here
 
-//    public static void createWriterObjects(String filepath) {
-//        try {
-//            fw = new FileWriter("results.csv", true);
-//        } catch (IOException e) {
-//            e.fillInStackTrace();
-//        }
-//        bw = new BufferedWriter(fw);
-//        pw = new PrintWriter(bw);
-//    }
-
     public void writeResultToFile(String description, long timeTaken) {
         try {
             fw = new FileWriter("results.csv", true);
@@ -39,25 +29,4 @@ public class CSVWriter {
         pw.flush();
         pw.close();
     }
-
-    /*public void writeResultsToFile(LinkedHashMap<String, String> results, String filePath) {
-        try {
-            int i = 0;
-            for(String result : results.keySet()) {
-                int index = result.indexOf(":") + 5;
-                String sortType = result.substring(0, index);
-                String sizeString = result.substring(index, index + 2);
-                String finString = i + "," + sortType + "," + sizeString + "," + results.get(result);
-                pw.println(finString);
-                pw.flush();
-            }
-            pw.close();
-            JOptionPane.showMessageDialog(null, "Records Saved.");
-
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Records failed to save.");
-            e.printStackTrace();
-        }
-    }*/
-
 }
